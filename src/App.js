68d59690eea92bd479f3eeb62ea66d3cb7591fbd
Ahmad Zuhair Rizqi <az.rizqi@gmail.com>
2022-11-28 22:11:06 +0700

    Feature zuhair: fix navbar and integrate login/register page with homepage (#13)
    * add staging/production
    
    * edit staging/production
    
    * edit2 staging/production
    
    * edit3 staging/production
    
    * edit4 staging/production
    
    * edit4 staging/production
    
    * edit_code_landing_page
    
    * fix deployment
    
    * fix deploy
    
    * integrate login/register page with homepage

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import Journey from "./pages/Journey";
 import Destination from "./pages/Destination";
 import Order from "./pages/Order";
 import UserDashboard from "./pages/UserDashboard";
-
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 function App() {
-
   return (
     <div className="App">
       <BrowserRouter>
@@ -19,7 +19,9 @@ function App() {
           <Route path="/Journey" element={<Journey />} />
           <Route path="/Destination" element={<Destination />} />
           <Route path="/Order" element={<Order />} />
-          <Route path="/profile" element={<UserDashboard/>} />
+          <Route path="/profile" element={<UserDashboard />} />
+          <Route path="/login" element={<SignIn />} />
+          <Route path="/register" element={<SignUp />} />
           <Route path="*" element={<h1>Page 404 Not Found</h1>} />
         </Routes>
       </BrowserRouter>
