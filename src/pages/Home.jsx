538e32d3e7d4f894d37c24f93346d44910f3a934
AhmadZRizqi <az.rizqi@gmail.com>
2022-11-28 20:49:04 +0700

    edit_code_landing_page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ResponsiveAppBar from "../components/Navbar";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Fab from "@mui/material/Fab";
@@ -12,6 +12,16 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import AppRegistrationIcon from "@mui/icons-material/AppRegistration";
 import Footer from "../components/Footer";
+import MarkunreadIcon from "@mui/icons-material/Markunread";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Typography from "@mui/material/Typography";
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
+import AirplaneTicketIcon from "@mui/icons-material/AirplaneTicket";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import Divider from "@mui/material/Divider";
 
 const theme = createTheme({
   palette: {
@@ -27,7 +37,43 @@ const theme = createTheme({
   },
 });
 
+// List Dummy
+function generate(element) {
+  return [0, 1, 2].map((value) =>
+    React.cloneElement(element, {
+      key: value,
+    })
+  );
+}
+
+const Root = styled("div")(({ theme }) => ({
+  paddingTop: theme.spacing(5),
+  width: "100%",
+  ...theme.typography.body2,
+  "& > :not(style) + :not(style)": {
+    marginTop: theme.spacing(5),
+  },
+}));
+
 function Home() {
+  const content = (
+    <div>
+      {
+        <a style={{ fontSize: 16 }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+          turpis sem, laoreet sit amet elementum quis, sagittis at purus. Mauris
+          vel orci posuere, venenatis arcu ut, rutrum nunc. Fusce orci velit,
+          maximus vel risus at, varius elementum lacus. Donec aliquam metus at
+          aliquet placerat. Nulla facilisi. Quisque volutpat auctor quam, sit
+          amet viverra arcu pulvinar vel. Cras non dui rutrum, posuere leo
+          ultricies, porta velit. Nunc sit amet arcu porttitor, maximus tellus
+          quis, facilisis ante. Ut convallis, libero ac elementum vulputate,
+          elit felis facilisis nisl, ut tristique sapien odio vitae massa.
+        </a>
+      }
+    </div>
+  );
+
   return (
     <ThemeProvider theme={theme}>
       <ResponsiveAppBar />
@@ -75,6 +121,100 @@ function Home() {
           />
         </Grid>
       </Grid>
+
+      <React.Fragment>
+        <Root>
+          <Divider>
+            Pesan tiket pesawat secara mudah dan berkualitas di Prime Flight
+          </Divider>
+          {content}
+          <Divider>
+            Kenapa kamu harus pesan tiket pesawat di Prime Flight?
+          </Divider>
+          {content}
+          <Divider />
+        </Root>
+      </React.Fragment>
+
+      <React.Fragment>
+        <Grid
+          container
+          spacing={0}
+          style={{
+            backgroundColor: "#DCDCDC",
+            textAlign: "start",
+            paddingLeft: 50,
+          }}
+        >
+          <Grid item xs={12} md={3}>
+            <Typography sx={{ mt: 4, mb: 1 }} variant="h5" component="div">
+              Prime Flight
+            </Typography>
+            <List>
+              <ListItem>
+                <ListItemIcon>
+                  <MarkunreadIcon />
+                </ListItemIcon>
+                <ListItemText primary="primeflight@gmail.com" />
+              </ListItem>
+
+              <ListItem>
+                <ListItemIcon>
+                  <LocalPhoneIcon />
+                </ListItemIcon>
+                <ListItemText primary="08XXXXXXXXXX" />
+              </ListItem>
+
+              <ListItem>
+                <ListItemIcon>
+                  <InstagramIcon />
+                </ListItemIcon>
+                <ListItemText primary="@primeflight" />
+              </ListItem>
+            </List>
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <Typography sx={{ mt: 4, mb: 1 }} variant="h5" component="div">
+              Journey
+            </Typography>
+            {generate(
+              <ListItem>
+                <ListItemIcon>
+                  <AirplaneTicketIcon />
+                </ListItemIcon>
+                <ListItemText primary="List Dummy" />
+              </ListItem>
+            )}
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <Typography sx={{ mt: 4, mb: 1 }} variant="h5" component="div">
+              Destination
+            </Typography>
+            {generate(
+              <ListItem>
+                <ListItemIcon>
+                  <AirplaneTicketIcon />
+                </ListItemIcon>
+                <ListItemText primary="List Dummy" />
+              </ListItem>
+            )}
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <Typography sx={{ mt: 4, mb: 1 }} variant="h5" component="div">
+              Order
+            </Typography>
+            {generate(
+              <ListItem>
+                <ListItemIcon>
+                  <AirplaneTicketIcon />
+                </ListItemIcon>
+                <ListItemText primary="List Dummy" />
+              </ListItem>
+            )}
+          </Grid>
+        </Grid>
+      </React.Fragment>
+
       <Footer />
     </ThemeProvider>
   );
--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -1,14 +1,162 @@
 import React from "react";
 import ResponsiveAppBar from "../components/Navbar";
 import Footer from "../components/Footer";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Checkbox from "@mui/material/Checkbox";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#3D3D3D",
+    },
+    secondary: {
+      main: "#656565",
+    },
+    sub: {
+      main: "#DCDCDC",
+    },
+  },
+});
 
 function Order() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <ResponsiveAppBar />
-      <div>Order</div>
+      <Grid
+        container
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Card
+          sx={{ maxWidth: 700 }}
+          style={{ marginTop: 10, marginBottom: 20 }}
+        >
+          <CardContent>
+            <Typography variant="h6" gutterBottom>
+              Order Registration
+            </Typography>
+            <Grid container spacing={3}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  id="firstName"
+                  name="firstName"
+                  label="First name"
+                  fullWidth
+                  autoComplete="given-name"
+                  variant="standard"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  id="lastName"
+                  name="lastName"
+                  label="Last name"
+                  fullWidth
+                  autoComplete="family-name"
+                  variant="standard"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  id="address1"
+                  name="address1"
+                  label="Address line 1"
+                  fullWidth
+                  autoComplete="shipping address-line1"
+                  variant="standard"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  id="address2"
+                  name="address2"
+                  label="Address line 2"
+                  fullWidth
+                  autoComplete="shipping address-line2"
+                  variant="standard"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  id="city"
+                  name="city"
+                  label="City"
+                  fullWidth
+                  autoComplete="shipping address-level2"
+                  variant="standard"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  id="state"
+                  name="state"
+                  label="State/Province/Region"
+                  fullWidth
+                  variant="standard"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  id="zip"
+                  name="zip"
+                  label="Zip / Postal code"
+                  fullWidth
+                  autoComplete="shipping postal-code"
+                  variant="standard"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  id="country"
+                  name="country"
+                  label="Country"
+                  fullWidth
+                  autoComplete="shipping country"
+                  variant="standard"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      color="secondary"
+                      name="saveAddress"
+                      value="yes"
+                    />
+                  }
+                  label="Use this address for payment details"
+                />
+              </Grid>
+            </Grid>
+          </CardContent>
+          <Button variant="contained" style={{ marginBottom: 10 }}>
+            Next
+          </Button>
+        </Card>
+      </Grid>
+
       <Footer />
-    </>
+    </ThemeProvider>
   );
 }
 
