8cdbbfcdfcff74ad1f00042b7bcdb55ca832ada6
Ahmad Zuhair Rizqi <az.rizqi@gmail.com>
2022-11-30 23:39:23 +0700

    Feature zuhair: integrate login/register with backend (#15)
    * add staging/production
    
    * edit staging/production
    
    * edit2 staging/production
    
    * edit3 staging/production
    
    * edit4 staging/production
    
    * edit4 staging/production
    
    * edit_code_landing_page
    
    * fix deployment
    
    * fix deploy
    
    * integrate login/register page with homepage
    
    * integrasi login/register

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import Home from "./pages/Home";
+import { Provider } from "react-redux";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { ToastContainer } from "react-toastify";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Journey from "./pages/Journey";
@@ -7,24 +10,31 @@ import Order from "./pages/Order";
 import UserDashboard from "./pages/UserDashboard";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import store from "./redux/store";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/">
-            <Route index element={<Home />} />
-          </Route>
-          <Route path="/Journey" element={<Journey />} />
-          <Route path="/Destination" element={<Destination />} />
-          <Route path="/Order" element={<Order />} />
-          <Route path="/profile" element={<UserDashboard />} />
-          <Route path="/login" element={<SignIn />} />
-          <Route path="/register" element={<SignUp />} />
-          <Route path="*" element={<h1>Page 404 Not Found</h1>} />
-        </Routes>
-      </BrowserRouter>
+      <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+        <Provider store={store}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/">
+                <Route index element={<Home />} />
+              </Route>
+              <Route path="/Journey" element={<Journey />} />
+              <Route path="/Destination" element={<Destination />} />
+              <Route path="/Order" element={<Order />} />
+              <Route path="/profile" element={<UserDashboard />} />
+              <Route path="/login" element={<SignIn />} />
+              <Route path="/register" element={<SignUp />} />
+              <Route path="*" element={<h1>Page 404 Not Found</h1>} />
+            </Routes>
+            <ToastContainer theme="colored" position="top-right" />
+          </BrowserRouter>
+        </Provider>
+      </GoogleOAuthProvider>
     </div>
   );
 }
